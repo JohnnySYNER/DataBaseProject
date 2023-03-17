ee1241f76dbd7bfa05bf720e0cb3fcd991dafd1e
Ciobanu Ion <ionutzse@gmail.com>
2023-03-17 12:57:16 +0200

    Created the new section {Customer} and added its functionalities as well as the new error message for NULL data Insertion

--- src/Database/Login.java
+++ src/Database/Login.java
@@ -153,7 +153,7 @@ public class Login extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
  
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-       
+      
         String sql = "SELECT * FROM login WHERE Username = ? AND Password = ?";
         try
         {
